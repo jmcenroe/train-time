a85b35501cf4d43dfcd8e6fb650c6f3f562bfcb3
Jarrod <jwmcenroe@gmail.com>
2017-10-13 22:52:24 -0700

    Completed logic to add to database and css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 
 	// Initialize Firebase
 	var config = {
-    apiKey: "AIzaSyAOPG-PEqF4VtNoUgziUq2yEZmSa9z87PA",
-    authDomain: "train-time-57f32.firebaseapp.com",
-    databaseURL: "https://train-time-57f32.firebaseio.com",
-    projectId: "train-time-57f32",
-    storageBucket: "",
-    messagingSenderId: "440278035187"
+	    apiKey: 'AIzaSyAOPG-PEqF4VtNoUgziUq2yEZmSa9z87PA',
+	    authDomain: 'train-time-57f32.firebaseapp.com',
+	    databaseURL: 'https://train-time-57f32.firebaseio.com',
+	    projectId: 'train-time-57f32',
+	    storageBucket: 'gtrain-time-57f32.appspot.com/',
+	    messagingSenderId: '440278035187'
 	};
 	firebase.initializeApp(config);
 
@@ -16,7 +16,8 @@ $(document).ready(function() {
 
 	// on button click, store data
 	$('#submit-btn').on('click', function(event){
-			//don't refresh the page
+		
+		//don't refresh the page
 	    event.preventDefault();
 
 	    // storing and retrieving the most recent information
@@ -26,14 +27,12 @@ $(document).ready(function() {
 	    var frequency = $('#frequency').val().trim();
 
 	    //clear input fields after submit
-
-	    $("#name").val("");
-	    $("#destination").val("");
-	    $("#firstTrain").val("");
-	    $("#frequency").val("");
+	    $('#name').val('');
+	    $('#destination').val('');
+	    $('#firstTrain').val('');
+	    $('#frequency').val('');
 
 	    //push data to add onto previous data
-
 	    dataRef.ref().push({
 	    	name:name,
 	    	destination:destination,
@@ -43,25 +42,55 @@ $(document).ready(function() {
 	});
 
 	// watch firebase for changes
-	dataRef.ref().on("child_added", function(childSnapshot) {
+	dataRef.ref().on('child_added', function(childSnapshot) {
         console.log(childSnapshot.val());
 
 
         //create new variables to call childSnapshot of data from firebase
-
         var name = childSnapshot.val().name;
         var destination = childSnapshot.val().destination;
         var frequency = childSnapshot.val().frequency;
         var time = childSnapshot.val().time;
         var key = childSnapshot.key;
 
-		//set a variable equal to the current time from moment.js
+        var firstTrainConverted = moment(time, "hh:mm").subtract(1, "years");
+        console.log(firstTrainConverted);
 
+		//set a variable equal to the current time from moment.js
         var currentTime = moment();
-        console.log("Current Time: " + moment(currentTime).format("hh:mm"));
+        console.log('Current Time: ' + moment(currentTime).format('hh:mm'));
 
         //post current time to jumbotron
+        $('#currentTime').html('Current Time: ' + moment(currentTime).format('hh:mm'));
 
-        $("#currentTime").html("Current Time: " + moment(currentTime).format("hh:mm"));
-        
-    });
+        //find the difference between the first train time and the current time
+        var timeDiff = moment().diff(moment(firstTrainConverted), 'minutes')
+        console.log('Time Difference: ' + timeDiff);
+
+        //find the time apart by finding the remainder of the time difference and the frequency - use modal to get whole remainder number
+
+        var timeRemainder = timeDiff % frequency;
+        console.log(timeRemainder);
+
+        //find the minutes until the next train
+
+        var nextTrainMin = frequency - timeRemainder;
+        console.log('Minutes Till Train: ' + nextTrainMin);
+
+        //find the time of the next train arrival
+
+        var nextTrainAdd = moment().add(nextTrainMin, 'minutes');
+        var nextTrainArr = moment(nextTrainAdd).format('hh:mm');
+        console.log('Arrival Time: ' + nextTrainArr);
+
+        //prepend all information for train data submitted by user
+
+        $('#schedule').prepend('<tr><td>' + name + '</td><td>' + destination + 
+        	'</td><td>' + frequency + '</td><td>' + nextTrainArr + '</td><td>' + 
+        	nextTrainMin + '</td><td>' + '</td></tr>');
+
+	    }, function(err) {
+	        console.log(err);
+	    });
+});
+
