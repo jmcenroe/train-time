88f7abc5ce57aa9d8805e49aee57a559c0902fc5
Ryan Hansen <rphansen91@gmail.com>
2017-10-24 18:26:29 -0700

    Update times

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
-	// Initialize Firebase
+  // Initialize clock
+  Clock();
+
+  // Initialize Firebase
 	var config = {
 	    apiKey: 'AIzaSyAOPG-PEqF4VtNoUgziUq2yEZmSa9z87PA',
 	    authDomain: 'train-time-57f32.firebaseapp.com',
@@ -16,7 +19,7 @@ $(document).ready(function() {
 
 	// on button click, store data
 	$('#submit-btn').on('click', function(event){
-		
+
 		//don't refresh the page
 	    event.preventDefault();
 
@@ -43,54 +46,69 @@ $(document).ready(function() {
 
 	// watch firebase for changes
 	dataRef.ref().on('child_added', function(childSnapshot) {
-        console.log(childSnapshot.val());
+    //create new variables to call childSnapshot of data from firebase
+    var train = childSnapshot.val();
+    var name = train.name;
+    var destination = train.destination;
+    var frequency = train.frequency;
+    var time = train.time;
+    var $arrival = $('<td>')
+    var $remaining = $('<td>')
 
+    function displayTrain () {
+      //prepend all information for train data submitted by user
+      var row = $('<tr>');
+      row.append($('<td>').text(name));
+      row.append($('<td>').text(destination));
+      row.append($('<td>').text(frequency));
+      row.append($arrival);
+      row.append($remaining);
+      $('#schedule').prepend(row);
+      updateTrain();
+    }
 
-        //create new variables to call childSnapshot of data from firebase
-        var name = childSnapshot.val().name;
-        var destination = childSnapshot.val().destination;
-        var frequency = childSnapshot.val().frequency;
-        var time = childSnapshot.val().time;
-        var key = childSnapshot.key;
+    function updateTrain () {
+      var firstTrainConverted = moment(time, "hh:mm").subtract(1, "years");
+      console.log(firstTrainConverted);
 
-        var firstTrainConverted = moment(time, "hh:mm").subtract(1, "years");
-        console.log(firstTrainConverted);
+      //find the difference between the first train time and the current time
+      var timeDiff = moment().diff(moment(firstTrainConverted), 'minutes')
+      console.log('Time Difference: ' + timeDiff);
 
-		//set a variable equal to the current time from moment.js
-        var currentTime = moment();
-        console.log('Current Time: ' + moment(currentTime).format('hh:mm'));
+      //find the time apart by finding the remainder of the time difference and the frequency - use modal to get whole remainder number
 
-        //post current time to jumbotron
-        $('#currentTime').html('Current Time: ' + moment(currentTime).format('hh:mm'));
+      var timeRemainder = timeDiff % frequency;
+      console.log(timeRemainder);
 
-        //find the difference between the first train time and the current time
-        var timeDiff = moment().diff(moment(firstTrainConverted), 'minutes')
-        console.log('Time Difference: ' + timeDiff);
+      //find the minutes until the next train
 
-        //find the time apart by finding the remainder of the time difference and the frequency - use modal to get whole remainder number
+      var nextTrainMin = frequency - timeRemainder;
+      console.log('Minutes Till Train: ' + nextTrainMin);
 
-        var timeRemainder = timeDiff % frequency;
-        console.log(timeRemainder);
+      //find the time of the next train arrival
 
-        //find the minutes until the next train
+      var nextTrainAdd = moment().add(nextTrainMin, 'minutes');
+      var nextTrainArr = moment(nextTrainAdd).format('hh:mm');
+      $arrival.text(nextTrainArr)
+      $remaining.text(nextTrainMin)
+      console.log('Arrival Time: ' + nextTrainArr);
+    }
 
-        var nextTrainMin = frequency - timeRemainder;
-        console.log('Minutes Till Train: ' + nextTrainMin);
-
-        //find the time of the next train arrival
-
-        var nextTrainAdd = moment().add(nextTrainMin, 'minutes');
-        var nextTrainArr = moment(nextTrainAdd).format('hh:mm');
-        console.log('Arrival Time: ' + nextTrainArr);
-
-        //prepend all information for train data submitted by user
-
-        $('#schedule').prepend('<tr><td>' + name + '</td><td>' + destination + 
-        	'</td><td>' + frequency + '</td><td>' + nextTrainArr + '</td><td>' + 
-        	nextTrainMin + '</td><td>' + '</td></tr>');
-
-	    }, function(err) {
-	        console.log(err);
-	    });
+    displayTrain();
+    setInterval(function () {
+      updateTrain();
+    }, 1000);
+  }, function(err) {
+      console.log(err);
+  });
 });
 
+function Clock () {
+  function tick () {
+    var currentTime = moment().format('hh:mm:ss');
+    $('#currentTime').html('Current Time: ' + currentTime);
+  }
+
+  setInterval(tick, 1000); // Update Clock Every Second
+  tick();
+}
